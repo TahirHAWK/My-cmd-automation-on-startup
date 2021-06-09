e765ba870b321c725bd5c2fbc62f4d2b2a66b865
TahirHSTU <45144696+TahirHSTU@users.noreply.github.com>
2021-06-09 18:24:12 +0600

    made a new file to start mongodb

--- my_current_work/my current work.cmd
+++ my_current_work/my current work.cmd
@@ -5,9 +5,7 @@ start /D "C:\Program Files (x86)\Google\Chrome\Application\" chrome.exe
 start /D "D:\PERSONAL\soft+games\soft (D)\study soft\Microsoft VS Code\" code.exe
 %SystemRoot%\explorer.exe "D:\PERSONAL\study\books\me\HSTU STUDY\online courses\Learn JavaScript Full-Stack from Scratch\5. Starting Our Complex App (App #2)"
 
-start /D "C:\Program Files\MongoDB Compass\" MongoDBCompass.exe
-
-mongod --dbpath=D:\xampp\htdocs\Complex_app_database
+ 
 
 
 
