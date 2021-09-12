d9389d5fadbf4df7adb65bcca9ab6def32ddd931
TahirHSTU <tahirtamin20@gmail.com>
2021-09-12 13:15:21 +0600

    Changed something

--- Startup/clearing_recent2.bat
+++ Startup/clearing_recent2.bat
@@ -1,5 +1,5 @@
 @echo off
-del /s /q "C:\Users\Tahir Anam Tamin\Recent\*.*"
-for /d %%p in ("C:\Users\Tahir Anam Tamin\Recent\*.*") do rmdir "%%p" /s /q
+del /s /q "C:\Users\USER PC\Recent\*.*"
+for /d %%p in ("C:\Users\USER PC\Recent\*.*") do rmdir "%%p" /s /q
  
 exit
--- Startup/clearing_temp2.cmd
+++ Startup/clearing_temp2.cmd
@@ -1,6 +1,6 @@
 @echo off
-del /s /q "C:\Users\TAHIRA~1\AppData\Local\Temp\*.*"
-for /d %%p in ("C:\Users\TAHIRA~1\AppData\Local\Temp\*.*") do rmdir "%%p" /s /q
+del /s /q "C:\Users\USERPC~1\AppData\Local\Temp\*.*"
+for /d %%p in ("C:\Users\USERPC~1\AppData\Local\Temp\*.*") do rmdir "%%p" /s /q
 
 exit
 
