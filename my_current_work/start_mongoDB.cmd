6cedec41c7cbb833b062e1f58c2ce750de27bcce
TahirHSTU <45144696+TahirHSTU@users.noreply.github.com>
2023-04-29 13:22:11 +0600

    added some notes on how to add environment variables using CMD
    This note will be used as direct command for people who don't know how to add environment variable for mongodb.

--- my_current_work/start_mongoDB.cmd
+++ my_current_work/start_mongoDB.cmd
@@ -1,3 +1,7 @@
-start /D "F:\my_urgent_worktools\MongoDB Compass" MongoDBCompass.exe
+start /D "F:\coding tools\installed" MongoDBCompass.exe
 
-mongod --dbpath=F:\my_urgent_worktools\Project_DB
+mongod --dbpath=F:\coding tools\mongodb_path  
+@REM "mongod --dbpath=F:\coding tools\mongodb_path  >text.txt" to take the output into a file
+
+@REM SETX PATH "C:\Program Files\MongoDB\Server\4.4\bin"
+@REM Using cmd set this path as an environment variable
