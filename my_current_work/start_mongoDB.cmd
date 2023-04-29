264afc2276a208c2af9cbc4765256c933e35a234
TahirHSTU <45144696+TahirHSTU@users.noreply.github.com>
2023-04-29 13:27:38 +0600

    added inverted comma on the --dbpath variable
    this comma must be added in order for the variable to work and add the data on the desired folder.

--- my_current_work/start_mongoDB.cmd
+++ my_current_work/start_mongoDB.cmd
@@ -1,7 +1,7 @@
 start /D "F:\coding tools\installed" MongoDBCompass.exe
 
-mongod --dbpath=F:\coding tools\mongodb_path  
-@REM "mongod --dbpath=F:\coding tools\mongodb_path  >text.txt" to take the output into a file
+mongod --dbpath="F:\coding tools\mongodb_path"  
+@REM "mongod --dbpath="F:\coding tools\mongodb_path"  >text.txt" to take the output into a file
 
 @REM SETX PATH "C:\Program Files\MongoDB\Server\4.4\bin"
 @REM Using cmd set this path as an environment variable
